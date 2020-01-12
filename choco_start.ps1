75f2aa4b5fbce2a76ef9f96f2d08717cdf790e1c
Alex Manoel Ferreira Silva <windolboy@yahoo.com.br>
2020-01-12 02:10:06 -0300

    Create choco_start.ps1

---
+++ choco_start.ps1
@@ -0,0 +1,35 @@
+# choco_start.ps1 by Alex Manoel Ferreira Silva
+# Get Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# See packages at https://chocolatey.org/packages/
+# Use according to your own needs...
+
+# Essentials
+choco install googlechrome -y
+choco install google-backup-and-sync -y
+choco install google-drive-file-stream -y
+
+# More Tools
+choco install powershell-core -y
+
+# Dev
+choco install wsl -y
+choco install git -y
+choco install tortoisegit -y
+choco install sourcetree -y
+choco install nodejs -y
+choco install android-sdk -y
+choco install androidstudio -y
+choco install sonyvegaspro -y
+
+# Games
+choco install discord -y
+choco install leagueoflegends -y
+
+# Others
+choco install whatsapp -y
+choco install ext2fsd -y
+choco install stremio -y
+
+# You´re done. ;)
