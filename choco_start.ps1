a695af0b413d4470f0d8e5fb0cf3a53b7132e3fa
Alex Manoel Ferreira Silva <windolboy@yahoo.com.br>
2020-01-13 10:38:36 -0300

    Update choco_start.ps1

--- choco_start.ps1
+++ choco_start.ps1
@@ -1,4 +1,19 @@
 # choco_start.ps1 by Alex Manoel Ferreira Silva
+
+# Create symbolic links
+New-Item -Path "C:\Users\windo\3D Objects" -ItemType SymbolicLink -Value "C:\Users\Dados\3D Objects"
+New-Item -Path "C:\Users\windo\Contacts" -ItemType SymbolicLink -Value "C:\Users\Dados\Contacts"
+New-Item -Path "C:\Users\windo\Desktop" -ItemType SymbolicLink -Value "C:\Users\Dados\Desktop"
+New-Item -Path "C:\Users\windo\Documents" -ItemType SymbolicLink -Value "C:\Users\Dados\Documents"
+New-Item -Path "C:\Users\windo\Downloads" -ItemType SymbolicLink -Value "C:\Users\Dados\Downloads"
+New-Item -Path "C:\Users\windo\Favorites" -ItemType SymbolicLink -Value "C:\Users\Dados\Favorites"
+New-Item -Path "C:\Users\windo\Links" -ItemType SymbolicLink -Value "C:\Users\Dados\Links"
+New-Item -Path "C:\Users\windo\Music" -ItemType SymbolicLink -Value "C:\Users\Dados\Music"
+New-Item -Path "C:\Users\windo\Pictures" -ItemType SymbolicLink -Value "C:\Users\Dados\Pictures"
+New-Item -Path "C:\Users\windo\Saved Games" -ItemType SymbolicLink -Value "C:\Users\Dados\Saved Games"
+New-Item -Path "C:\Users\windo\Searches" -ItemType SymbolicLink -Value "C:\Users\Dados\Searches"
+New-Item -Path "C:\Users\windo\Videos" -ItemType SymbolicLink -Value "C:\Users\Dados\Videos"
+
 # Get Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
