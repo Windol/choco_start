320303824aa7b409363272acf922b5ec5b8b8b63
Alex Manoel Ferreira Silva <windolboy@yahoo.com.br>
2020-06-01 07:45:26 -0300

    Update choco_start.ps1

--- choco_start.ps1
+++ choco_start.ps1
@@ -31,8 +31,8 @@ New-Item -Path "C:\Users\windo\.emulator_console_auth_token" -ItemType SymbolicL
 New-Item -Path "C:\Users\windo\.gitconfig" -ItemType SymbolicLink -Value "C:\Users\Dados\.gitconfig"
 New-Item -Path "C:\Users\windo\.jgitconfig" -ItemType SymbolicLink -Value "C:\Users\Dados\.jgitconfig"
 New-Item -Path "C:\Users\windo\_netrc" -ItemType SymbolicLink -Value "C:\Users\Dados\_netrc"
-New-Item -Path "C:\Users\windo\.AndroidStudio3.6" -ItemType SymbolicLink -Value "C:\Users\Dados\.AndroidStudio3.6"
 New-Item -Path "C:\Users\windo\AppData\Roaming\Factorio" -ItemType SymbolicLink -Value "C:\Users\Dados\Jogos\Factorio Roaming"
+New-Item -Path "C:\Users\windo\.AndroidStudio4.0" -ItemType SymbolicLink -Value "C:\Users\Dados\.AndroidStudio4.0"
 
 # Get Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
