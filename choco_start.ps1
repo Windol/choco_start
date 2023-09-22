f7ab50ac23974b88e2a1bf9b4aea591245bce048
Alex Manoel Ferreira Silva <windolboy@yahoo.com.br>
2023-09-22 16:32:45 -0300

    Update choco_start.ps1

--- choco_start.ps1
+++ choco_start.ps1
@@ -48,6 +48,8 @@ New-Item -Path "C:\Users\windo\AppData\Roaming\Factorio" -ItemType SymbolicLink 
 # New-Item -Path "C:\Program Files\Docker" -ItemType SymbolicLink -Value "C:\Users\Dados\Program Files\Docker"
 # New-Item -Path "C:\Program Files (x86)\Microsoft Visual Studio" -ItemType SymbolicLink -Value "C:\Users\Dados\Program Files (x86)\Microsoft Visual Studio"
 New-Item -Path "C:\Program Files\Siemens" -ItemType SymbolicLink -Value "C:\Users\Dados\Program Files\Siemens"
+New-Item -Path "C:\Program Files (x86)\Common Files\Siemens" -ItemType SymbolicLink -Value "C:\Users\Dados\Program Files (x86)\Common Files\Siemens"
+New-Item -Path "C:\ProgramData\Siemens" -ItemType SymbolicLink -Value "C:\Users\Dados\ProgramData\Siemens"
 # New-Item -Path "C:\Program Files\Microsoft Visual Studio" -ItemType SymbolicLink -Value "C:\Users\Dados\Program Files\Microsoft Visual Studio"
 # New-Item -Path "C:\Users\windo\AppData\Local\Packages" -ItemType SymbolicLink -Value "C:\Users\Dados\AppData\Local\Packages"
 # New-Item -Path "C:\Users\windo\AppData\Local\Autodesk" -ItemType SymbolicLink -Value "C:\Users\Dados\AppData\Local\Autodesk"
